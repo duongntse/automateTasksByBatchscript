1cbe4563c37901e92e30ac8a48a243f988d4c56d
harry <duongntse@gmail.com>
2020-12-13 06:50:05 +0700

    updated items.js

--- auto_crawl_deploy/start_python_crawling.bat
+++ auto_crawl_deploy/start_python_crawling.bat
@@ -1,3 +1,3 @@
 @REM set pythonCrawlingLatestComicsPath="/mnt/c/users/duongntse/desktop/vscode_projects/python/web-crawling/latestchaptercomic/latestchaptercomic"
-set pythonCrawlingLatestComicsPath="../../latestChapterComic"
+set pythonCrawlingLatestComicsPath="../../latestChapterComic/latestchaptercomic/"
 bash -c "cd %pythonCrawlingLatestComicsPath% && python3 run.py" 
